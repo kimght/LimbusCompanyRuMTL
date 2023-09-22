81483b08de18d2d59663468a962a793d327893e3
Knightey <103951420+1Knightey1@users.noreply.github.com>
2023-09-23 02:42:22 +0300

    v.0.1.5

--- src/LCB_LCBRMod.cs
+++ src/LCB_LCBRMod.cs
@@ -1,39 +1,41 @@
-﻿using LimbusLocalizeRUS;
-using MelonLoader;
+﻿using BepInEx;
+using BepInEx.Configuration;
+using BepInEx.Unity.IL2CPP;
 using System;
 using System.IO;
 using System.Reflection;
-using System.Text.RegularExpressions;
 using UnityEngine;
 
-[assembly: MelonInfo(typeof(LCB_LCBRMod), LCB_LCBRMod.NAME, LCB_LCBRMod.VERSION, LCB_LCBRMod.AUTHOR, LCB_LCBRMod.LCBRLink)]
 namespace LimbusLocalizeRUS
 {
-    public class LCB_LCBRMod : MelonMod
+    [BepInPlugin(GUID, NAME, VERSION)]
+    public class LCB_LCBRMod : BasePlugin
     {
+        public static ConfigFile LCBR_Settings;
         public static string ModPath;
         public static string GamePath;
+        public const string GUID = "Com.BrightNKnightey.LocalizeLimbusCompanyRUS";
         public const string NAME = "LimbusCompanyBusRUS";
-        public const string VERSION = "0.1.3";
-        public const string AUTHOR = "Base: Bright\nRUS version: Knightey";
+        public const string VERSION = "0.1.5";
+        public const string AUTHOR = "Base: Bright\nRUS version: Knightey, abcdcode";
         public const string LCBRLink = "https://github.com/Crescent-Corporation/LimbusCompanyBusRUS";
-        public static MelonPreferences_Category LCBR_Settings = MelonPreferences.CreateCategory("LCBR", "LCBR Settings");
         public static Action<string, Action> LogFatalError { get; set; }
         public static Action<string> LogError { get; set; }
         public static Action<string> LogWarning { get; set; }
-        public static void OpenLCBRURL() { Application.OpenURL(LCBRLink); }
-        public static void OpenGamePath() { Application.OpenURL(GamePath); }
-        public override void OnInitializeMelon()
+        public static void OpenLCBRURL() => Application.OpenURL(LCBRLink);
+        public static void OpenGamePath() => Application.OpenURL(GamePath);
+        public override void Load()
         {
-            LogError = (string log) => { LoggerInstance.Error(log); Debug.LogError(log); };
-            LogWarning = (string log) => { LoggerInstance.Warning(log); Debug.LogWarning(log); };
+            LCBR_Settings = Config;
+            LogError = (string log) => { Log.LogError(log); Debug.LogError(log); };
+            LogWarning = (string log) => { Log.LogWarning(log); Debug.LogWarning(log); };
             LogFatalError = (string log, Action action) => { LCBR_Manager.FatalErrorlog += log + "\n"; LogError(log); LCBR_Manager.FatalErrorAction = action; LCBR_Manager.CheckModActions(); };
             ModPath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
             GamePath = new DirectoryInfo(Application.dataPath).Parent.FullName;
+            LCBR_UpdateChecker.StartAutoUpdate();
             try
             {
-                LCBR_UpdateChecker.StartCheckUpdates();
-                HarmonyLib.Harmony harmony = new("LimbusLocalizeRUS");
+                HarmonyLib.Harmony harmony = new(NAME);
                 if (LCBR_Russian_Settings.IsUseRussian.Value)
                 {
                     LCBR_Manager.InitLocalizes(new DirectoryInfo(ModPath + "/Localize/RU"));
@@ -46,23 +48,16 @@ namespace LimbusLocalizeRUS
                 harmony.PatchAll(typeof(LCBR_Russian_Settings));
                 if (!LCB_Cyrillic_Font.AddCyrillicFont(ModPath + "/tmpcyrillicfonts"))
                     LogFatalError("You have forgotten to install Font Update Mod. Please, reread README on Github.", OpenLCBRURL);
-                foreach (var font in LCB_Cyrillic_Font.tmpcyrillicfontsnames)
-                {
-                    LoggerInstance.Msg(font);
-                }
             }
             catch (Exception e)
             {
-                LogFatalError("Mod has met an unknown fatal error! Contact us on Github with the log, please.", () => { OnApplicationQuit(); OpenGamePath(); OpenLCBRURL(); });
+                LogFatalError("Mod has met an unknown fatal error! Contact us through urls in Github with the log, please.", () => { CopyLog(); OpenGamePath(); OpenLCBRURL(); });
                 LogError(e.ToString());
             }
         }
-        public override void OnApplicationQuit()
+        public static void CopyLog()
         {
-            File.Copy(GamePath + "/MelonLoader/Latest.log", GamePath + "/Latest.log", true);
-            var Latestlog = File.ReadAllText(GamePath + "/Latest.log");
-            Latestlog = Regex.Replace(Latestlog, "[0-9:\\.\\[\\]]+ During invoking native->managed trampoline(\r\n)?", "");
-            File.WriteAllText(GamePath + "/Latest.log", Latestlog);
+            File.Copy(GamePath + "/BepInEx/LogOutput.log", GamePath + "/Latest.log", true);
             File.Copy(Application.consoleLogPath, GamePath + "/Player.log", true);
         }
     }
